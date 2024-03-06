383213a4bfb51469b62c11acbb98402cc4e67c6f
matheus-wisniewski <maahwiski@gmail.com>
2024-03-06 04:10:36 -0300

    setup inicial do projeto. Cria todas as rotas e os componentes header e main. Também cria um CSS inicial para eles.

--- matheus-wisniewski.github.io/src/App.tsx
+++ matheus-wisniewski.github.io/src/App.tsx
@@ -1,9 +1,19 @@
-import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import './index.css'
+import Hero from './pages/Hero'
+import Projects from './pages/Projects'
+import Contact from './pages/Contact'
+import About from './pages/About'
 
 function App() {
   return (
     <>
-      <h1>Matheus.</h1>
+      <Routes>
+        <Route path="/" element={<Hero />} />
+        <Route path="/projetos" element={<Projects />} />
+        <Route path="/contato" element={<Contact />} />
+        <Route path="/sobre" element={<About />} />
+      </Routes>
     </>
   )
 }
---
+++ matheus-wisniewski.github.io/src/pages/Hero.tsx
@@ -0,0 +1,31 @@
+import { NavLink } from 'react-router-dom';
+import './pages-css/Hero.css'
+
+function Hero() {
+  return (
+    <>
+    <header>
+      <h1>Matheus.</h1>
+      <nav className="hero-nav">
+        <NavLink to="/projetos"> Projetos </NavLink>
+        <NavLink to="/sobre"> Sobre </NavLink>
+        <NavLink to="/contato"> Contato </NavLink>
+        <NavLink to='/'> Home </NavLink>
+    </nav>
+    </header>
+
+    <main className="hero-main">
+      <section className="hero-section">
+        <h2>Olá, eu sou o Matheus</h2>
+        <p>Desenvolvedor Front-End</p>
+      </section>
+    </main>
+
+    <footer className="hero-footer">
+      <p>Desenvolvido por Matheus</p>
+    </footer>
+    </>
+  );
+}
+
+export default Hero;
